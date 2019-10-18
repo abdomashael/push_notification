9d4c7952b5f8070ad782c7e7aff64f45192ab3cb
abdomashael <abdomashael@yahoo.com>
2019-10-18 14:43:06 +0200

    reformat code

--- routes/index.js
+++ routes/index.js
@@ -4,38 +4,42 @@ var bodyParser = require('body-parser');
 var FCM = require('fcm-push');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    //res.render('index', { title: 'Express' });
 
 
-  var serverKey = req.headers.key;
-  var fcm = new FCM(serverKey);
+    var serverKey = req.headers.key;
+    var fcm = new FCM(serverKey);
 
-  var message = {
-    to: req.headers.to, // required fill with device token or topics
-    collapse_key: 'your_collapse_key',
-    data: {
-      title:req.headers.title,
-      body: req.headers.body,
-      info: req.headers.info
-    },
-    notification: {
-      title: req.headers.title,
-      body: req.headers.body
-    }
-  };
+    var message = {
+        to: req.headers.to, // required fill with device token or topics
+        collapse_key: 'your_collapse_key',
+        data: {
+            title: req.headers.title,
+            body: req.headers.body,
+            info: req.headers.info
+        },
+        notification: {
+            title: req.headers.title,
+            body: req.headers.body
+        }
+    };
 
 //promise style
-  fcm.send(message)
-      .then(function(response){
-        res.send({ status: "Successfully sent with response: ",
-          details:  response});
-      })
-      .catch(function(err){
-        res.send({ status: "Something has gone wrong! ",
-          error:  err});
+    fcm.send(message)
+        .then(function (response) {
+            res.send({
+                status: "Successfully sent with response: ",
+                details: response
+            });
+        })
+        .catch(function (err) {
+            res.send({
+                status: "Something has gone wrong! ",
+                error: err
+            });
 
-      })
+        })
 });
 
 module.exports = router;
